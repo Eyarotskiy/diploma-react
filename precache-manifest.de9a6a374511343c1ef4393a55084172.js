76e3d7388b8ee32c6306d2905829d5017812ebd8
Eyarotskiy <eyarotskiy94@gmail.com>
2020-03-16 02:06:28 +0100

    initial commit

---
+++ precache-manifest.de9a6a374511343c1ef4393a55084172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303012345cebcc0c41b703514f23c500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e36c62b92a2cd0e261f",
+    "url": "/static/css/main.740973bb.chunk.css"
+  },
+  {
+    "revision": "c2ee51d8a8cb5e79287e",
+    "url": "/static/js/2.0615c41d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0615c41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e36c62b92a2cd0e261f",
+    "url": "/static/js/main.71a58562.chunk.js"
+  },
+  {
+    "revision": "9ba3358706b6c2fd4c60",
+    "url": "/static/js/runtime-main.310431d5.js"
+  }
+]);
